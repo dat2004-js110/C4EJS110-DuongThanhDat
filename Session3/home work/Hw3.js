7127b885c4a9e6390526a881936d4c6d611d980d
unknown <you@example.com>
2021-03-23 20:48:36 +0700

    Update

--- Session3/home work/Hw3.js
+++ Session3/home work/Hw3.js
@@ -77,10 +77,10 @@ const a = [4, 5, 7, -8];
 }*/
 
 //Ex 6: Write a script asking users to enter a sequence of numbers, the numbers are separated by commas, find the smallest number and log it out to users
-{
+/*{
     let listNum = prompt("Nhập 1 dãy số, mỗi số ngăn cách nhau bằng dấu phẩy(,)");
     let aNum = listNum.split(",");
-}
+}*/
 //Ex 7: Create an array containing at least 5 numbers, then ask users enter a number, perform a search to look for the number in the array, if the number is found, tell user that with the index of it in the array, if not, also tell them so
 /*{
     const arr = [3, 4, 6, -9, 10, -88, 2];
